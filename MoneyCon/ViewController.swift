ca8c84f4704c4f4fa3bdc5cc7529e0683b7e359c
Farid Ganbarli <faridganbarli@farids-macbook-air.local>
2019-01-18 19:35:49 +0100

    Initial Commit

---
+++ MoneyCon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoneyCon
+//
+//  Created by Farid Ganbarli on 2019. 01. 18..
+//  Copyright © 2019. Farid Ganbarli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
