d2fe8874688f2be4fcab6e33055566cf60cda7f2
Farid Ganbarli <faridganbarli@Farids-MacBook-Air.local>
2019-01-19 17:15:56 +0100

    Entry Page is finished

---
+++ MoneyCon/CustomTextField.swift
@@ -0,0 +1,21 @@
+//
+//  CustomTextField.swift
+//  MoneyCon
+//
+//  Created by Farid Ganbarli on 2019. 01. 19..
+//  Copyright © 2019. Farid Ganbarli. All rights reserved.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
